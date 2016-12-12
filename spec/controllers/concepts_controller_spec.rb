140610cbbf72f27fd9c3819695cb9bd430f21cf8
Eddie Glenn <eglenn@mpr.org>
2016-12-12 11:46:05 -0600

    add a couple of tests to concepts model

--- spec/controllers/concepts_controller_spec.rb
+++ spec/controllers/concepts_controller_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ConceptsController, type: :controller do
+
+  let(:valid_attributes) {
+    {name: 'name',
+    summary: 'summary'}
+  }
+
+  # let(:invalid_attributes) {
+  #   skip("Add a hash of attributes invalid for your model")
+  # }
+
   describe 'GET index' do
     it 'renders index template' do
-      get :index
+      concept = Concept.create! valid_attributes
+      get :index, params: {}
       expect(response).to render_template('index')
+      expect(assigns(:concepts)).to eq([concept])
     end
   end
 
@@ -14,4 +26,5 @@ RSpec.describe ConceptsController, type: :controller do
       expect(assigns(:concept)).to be_a_new(Concept)
     end
   end
+
 end
--- spec/models/concept_spec.rb
+++ spec/models/concept_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Concept, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with name" do
+    concept = described_class.new(name: 'Anything')
+    expect(concept).to be_valid
+  end
+
+  it "is not valid with no name" do
+    concept = described_class.new(name: nil)
+    expect(concept).not_to be_valid
+  end
+
 end
