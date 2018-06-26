d60753e7a5cf9f6b7f10f6cb105a013ccd7da48f
Eddie <glen0071@gmail.com>
2018-06-26 00:10:12 -0500

    make better thing

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ActiveRecord::Base
   has_many :references
   has_many :concepts, through: :references
   belongs_to :author
+  belongs_to :writing
 
   validates :text, length: { minimum: 5, too_short: 'Quote cannot be less than 5 characters long' }
 end
